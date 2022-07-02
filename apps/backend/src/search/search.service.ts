07e0572a28c27f68b69f66ebb6ece0c40d59c57b
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2022-07-02 15:02:26 +0200

    Add search service

---
+++ apps/backend/src/search/search.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class SearchService {
+  create() {
+    // TODO: Implement this?
+  }
+
+  search() {
+    // TODO: Implement this?
+  }
+}
